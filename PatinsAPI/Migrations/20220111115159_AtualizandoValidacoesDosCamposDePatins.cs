477bce3c5723e7bbd96fa1e23b5c83f13b65d2e5
Matheus Martins <math.fmartins42@gmail.com>
2022-01-11 09:06:29 -0300

    Validações das Propriedades de Patins atualizadas. -> Add-Migration AtualizandoValidacoesDosCamposDePatins; -> Update-Database

---
+++ PatinsAPI/Migrations/20220111115159_AtualizandoValidacoesDosCamposDePatins.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PatinsAPI.Migrations
+{
+    public partial class AtualizandoValidacoesDosCamposDePatins : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Patins",
+                type: "varchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Modelo",
+                table: "Patins",
+                type: "varchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Marca",
+                table: "Patins",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Patins",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Modelo",
+                table: "Patins",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Marca",
+                table: "Patins",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
