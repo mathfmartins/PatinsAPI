10a7532d002f322cfbcbf03eb2c220b186fa95e1
Matheus Martins <math.fmartins42@gmail.com>
2022-01-11 07:09:38 -0300

    Adicionar arquivos de projeto.

---
+++ PatinsAPI/Controllers/PatinsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PatinsAPI.Models;
+
+namespace PatinsAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PatinsController : ControllerBase
+    {
+        private static Patins _patins = new Patins();
+
+        public IActionResult AddPatins()
+    }
+}
---
+++ PatinsAPI/Models/Patins.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatinsAPI.Models
+{
+    public class Patins
+    {
+        public string Nome { get; set; }
+        public int Numero { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+    }
+}
