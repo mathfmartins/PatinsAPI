d0db782a99378db5bb28ccbf57fbff34002a7b26
Matheus Martins <math.fmartins42@gmail.com>
2022-01-11 08:29:43 -0300

    Modelo mapeado para o banco de dados MySQL. -> Add-Migration CriandoTabelaPatins; -> Update-Database ;

---
+++ PatinsAPI/Migrations/20220111112744_CriandoTabelaPatins.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace PatinsAPI.Migrations
+{
+    public partial class CriandoTabelaPatins : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Modelo = table.Column<string>(type: "text", nullable: false),
+                    Marca = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patins", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patins");
+        }
+    }
+}
