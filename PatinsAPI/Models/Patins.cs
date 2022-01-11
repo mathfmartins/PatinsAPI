bcf8669be5a8cd0f1bb6d4ee82a33a72c6ff05c1
Matheus Martins <math.fmartins42@gmail.com>
2022-01-11 08:26:27 -0300

    Validações nas propriedades de Patins.

--- PatinsAPI/Models/Patins.cs
+++ PatinsAPI/Models/Patins.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,16 @@ namespace PatinsAPI.Models
 {
     public class Patins
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Nome deve ser informado.")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Numero deve ser informado.")]
         public int Numero { get; set; }
+        [Required(ErrorMessage = "Modelo deve ser informado.")]
         public string Modelo { get; set; }
+        [Required(ErrorMessage = "Marca deve ser informada")]
         public string Marca { get; set; }
     }
 }
