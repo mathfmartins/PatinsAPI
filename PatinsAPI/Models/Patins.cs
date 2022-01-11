165a25841b0c7019be5804add48b9aaa9287ea12
Matheus Martins <math.fmartins42@gmail.com>
2022-01-11 09:06:54 -0300

    Validações de Patins refatorado.

--- PatinsAPI/Models/Patins.cs
+++ PatinsAPI/Models/Patins.cs
@@ -12,12 +12,16 @@ namespace PatinsAPI.Models
         [Required]
         public int Id { get; set; }
         [Required(ErrorMessage = "Nome deve ser informado.")]
+        [StringLength(30, ErrorMessage = "Nome deve ter no máximo 30 caracteres.")]
         public string Nome { get; set; }
         [Required(ErrorMessage = "Numero deve ser informado.")]
+        [Range(20, 45, ErrorMessage = "Número deve estar entre 20 e 45.")]
         public int Numero { get; set; }
         [Required(ErrorMessage = "Modelo deve ser informado.")]
+        [StringLength(30, ErrorMessage = "Modelo deve ter no máximo 30 caracteres.")]
         public string Modelo { get; set; }
-        [Required(ErrorMessage = "Marca deve ser informada")]
+        [Required(ErrorMessage = "Marca deve ser informada.")]
+        [StringLength(20, ErrorMessage = "Marca deve ter no máximo 20 caracteres")]
         public string Marca { get; set; }
     }
 }
